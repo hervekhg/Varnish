84b67c137600bd6fc6f71c45da1c3ae8016b3261
Herve <herve.esir@gmail.com>
2016-09-13 15:53:18 +0200

    Create boucles.rb

---
+++ boucles.rb
@@ -0,0 +1,15 @@
+
+users = ["alice","bob","herve","loic"]
+
+i = 0
+users.each do |user|
+    puts i
+    puts user
+    i = i + 1
+end
+
+10.times do |i|
+  puts "Je suis fort #{i}"
+  i.times do
+
+end
